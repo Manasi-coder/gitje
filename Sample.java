c76e014bd334d66d87dffaab2944546726c4e154
manasi <you@example.com>
2022-08-26 12:08:00 +0530

    modified

--- Sample.java
+++ Sample.java
@@ -1,6 +1,10 @@
 import java.util.*;
-class Sample{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
+public class Sample 
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
 }  
